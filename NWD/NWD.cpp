1ba7f28017a2ff579d8e3e7d50adfa93f0caa554
bszczepanik235 <bszczepanik235@technikumkreatywne.pl>
2022-03-17 09:12:13 +0100

    Dodano algorytm NWD

---
+++ NWD/NWD.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> a;
+
+    cout << "Podaj drugą liczbe: ";
+    cin >> b;
+
+    while (a != b) {
+        if (a < b) {
+            b = b - a;
+        }
+        else {
+            a = a - b;
+        }
+    }
+
+    cout << "Najwiekszy wspolny dzielnik: " << a;
+}
