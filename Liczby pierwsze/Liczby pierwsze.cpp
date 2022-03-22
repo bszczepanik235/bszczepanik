51a287e3e5c2434350e9d5ecf0b71300bfb3d5ae
Bartek Szczepanik <bszczepanik235@technikumkreatywne.pl>
2022-03-22 21:18:52 +0100

    Zaaktualizowanie kodu liczb pierwszych

--- Liczby pierwsze/Liczby pierwsze.cpp
+++ Liczby pierwsze/Liczby pierwsze.cpp
@@ -4,13 +4,30 @@ using namespace std;
 int main()
 {
     int lp = 0;
-    int n;
+    int p = 0;
+    int n = 2;
+    int d = 2;
 
     cout << "Ile liczb pierwszych chcesz wyswietlic: ";
-    cin >> n;
+    cin >> lp;
+    cout << endl << "Liczby pierwsze: ";
 
-    while (lp < n) {
-        for(int )
+    while(lp < n){
+        while(d < n){
+            if(n % d == 0){
+                d = 2;
+                n++;
+                break;
+            }
+            d++;
+        }
     }
+    if(d >= n){
+        cout << n << ",";
+        d = 2;
+        n++;
+        lp++;
+    }
+
 }
 
