fa6d9768437a2c75bebecaf220e73e146f2659f4
bszczepanik235 <bszczepanik235@technikumkreatywne.pl>
2022-04-21 09:04:31 +0200

    Ciąg Fibonacciego

---
+++ Ciąg Fibonacciego/Ciąg Fibonacciego.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, f0, f1, f;
+
+    cout << "Ile wyrazow ciagu wypisac: ";
+    cin >> n;
+
+    f0 = 0;
+    f1 = 1;
+
+    for (int i = 0; i <= n; i++) {
+        if (i > 1) {
+            f = f0 + f1;
+            f0 = f1;
+            f1 = f;
+        }
+        else {
+            f = i;
+        }
+        cout << f << endl;
+    }
+
+}
+
