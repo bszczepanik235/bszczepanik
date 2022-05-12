1b3544aa985313658c7a0aa6fb934dc22cbc8c8b
bszczepanik235 <bszczepanik235@technikumkreatywne.pl>
2022-05-12 09:17:55 +0200

    Wyszukiwanie binarne

---
+++ Wyszukiwanie binarne/Wyszukiwanie binarne.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+int funckja(int szukana, int tab[]);
+int main()
+{
+	
+	int tab[15] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
+
+	int szukana;
+	cout << "Podaj liczbe ktora chcesz znalezc: ";
+	cin >> szukana;
+	funkcja(szukana, tab);
+}
+
+int funkcja(int szukana, int tab[]) 
+{
+	int l, p, sr;
+
+	l = 0;
+	p = 15;
+	sr = (l + p) / 2;
+
+	while (l <= p) {
+		if (tab[sr] == szukana) {
+			return sr;
+		}
+
+		if (tab[sr] > szukana) {
+			p = sr - 1;
+		}
+		else {
+			l = sr + 1;
+		}
+		sr = (l + p) / 2;
+	}
+
+	return -1;
+	cout << "Liczba " << szukana << "wystepuje w komorce o indeksie" << pozycja;
+}
